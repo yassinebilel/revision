0349b8f8c7543405c7a6bab7f5ec68f0fe1e5c18
nextwarrior000 <yassinesalem543@gmail.com>
2023-12-16 12:10:12 +0100

    updated

---
+++ index.js
@@ -0,0 +1,630 @@
+function iterateAndLog(number){
+    var result=0
+    for (let i = 0; i <=number ; i++) {
+        if(number%2===0){
+            return number+'is even'
+        }
+        return number+'is odd'
+        
+    }
+}
+undefined
+iterateAndLog(2)
+'2is even'
+function iterateAndLog(number){
+    for (let i = 0; i <=number ; i++) {
+        if(number%2===0){
+            return number+'is even'
+        }
+        return number+'is odd'
+        
+    }
+}
+undefined
+iterateAndLog(2)
+'2is even'
+iterateAndLog(5)
+'5is odd'
+function iterateAndLog(number){
+    for (let i = 0; i <=number ; i++) {
+        if(number%2===0){
+            return number+' is even'
+        }
+        return number+' is odd'
+        
+    }
+}
+undefined
+iterateAndLog(5)
+'5 is odd'
+function iterateAndLog(number){
+    let i=0
+    while ( i <=number) i++
+        n++{
+        if(number%2===0){
+            return number+' is even'
+        }
+        return number+' is odd'
+        
+    }
+}
+VM363:4 Uncaught SyntaxError: Unexpected token '{'
+function iterateAndLog(number){
+    let i=0
+    while ( i <=number){ i++}
+        n++
+        {
+            
+        if(number%2===0){
+            return number+' is even'
+        }
+        return number+' is odd'
+        
+    }
+}
+undefined
+iterateAndLog(5)
+VM369:4 Uncaught ReferenceError: n is not defined
+    at iterateAndLog (<anonymous>:4:9)
+    at <anonymous>:1:1
+iterateAndLog @ VM369:4
+(anonymous) @ VM375:1
+function iterateAndLog(number){
+    let i=0
+    while ( i <=number){ i++}
+        
+        {
+            
+        if(number%2===0){
+            return number+' is even'
+        }
+        return number+' is odd'
+        
+    }
+}
+undefined
+iterateAndLog(5)
+'5 is odd'
+function iterateAndLog(number){
+    let i=0
+    while ( i <=number){ i++}
+        
+        {
+            
+        if(number%2===0){
+            return number+' is even'
+        }
+        return number+' is odd'
+        
+    }
+}
+undefined
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+    }
+       return iterateAndLog(number-1)
+}
+    
+undefined
+iterateAndLog(5)
+'4 is even'
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+    }
+       return iterateAndLog(number)
+}
+    
+undefined
+iterateAndLog(5)
+VM535:2 Uncaught RangeError: Maximum call stack size exceeded
+    at iterateAndLog (<anonymous>:2:5)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+    at iterateAndLog (<anonymous>:5:15)
+iterateAndLog @ VM535:2
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+iterateAndLog @ VM535:5
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+    }
+       return iterateAndLog(number-1)
+}
+    
+undefined
+iterateAndLog(45)
+'44 is even'
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+        return number+' is odd'
+    }
+       return iterateAndLog(number-1)
+}
+    
+undefined
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+        else if(number%2==0){
+        return number+' is odd'
+        }
+        
+    }
+       return iterateAndLog(number-1)
+}
+    
+VM724:4 Uncaught SyntaxError: Unexpected token 'else'
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+            
+        }
+    else if(number%2==0)
+        {
+        return number+' is odd'
+        }
+        
+    }
+       return iterateAndLog(number-1)
+}
+    
+VM747:12 Uncaught SyntaxError: Illegal return statement
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is even'
+            
+        }
+    else if(number%2!==0)
+        {
+        return number+' is odd'
+        }
+        
+    }
+       return iterateAndLog(number-1)
+}
+    
+VM751:12 Uncaught SyntaxError: Illegal return statement
+function iterateAndLog(number){
+    if(number%2===0){
+        return number+' is odd'
+        
+        
+        }
+         
+        else  number+' is even'
+    }
+       return iterateAndLog(number-1)
+}
+    
+VM783:10 Uncaught SyntaxError: Illegal return statement
+function weirdDivisionWithFor(n) {
+for (let i = 1; i < n; i++) {
+    if (n%3===0){
+        return "julia"
+    }
+    else if (n%5===0){
+        return "james"
+    }
+    else if(n%3===0 && n%5===0){
+        return "juliajames"
+    }
+    else if(n%3!==0 || n%5!==0)
+        return n
+}
+
+}
+
+undefined
+weirdDivisionWithFor(16)
+16
+weirdDivisionWithFor(140)
+'james'
+let i=0
+while(i<n){
+    i++
+}
+VM1085:1 Uncaught SyntaxError: Identifier 'i' has already been declared
+function weirdDivisionWithFor(n)
+
+let i=0
+while(i<n){
+    i++
+}
+ if (n%3===0){
+        return "julia"
+    }
+    else if (n%5===0){
+        return "james"
+    }
+    else if(n%3===0 && n%5===0){
+        return "juliajames"
+    }
+    else if(n%3!==0 || n%5!==0)
+        return n
+}
+
+}
+VM1096:3 Uncaught SyntaxError: Unexpected identifier 'let'
+function weirdDivisionWithFor(n)
+
+let i=1
+while(i<n){
+    i++
+}
+ if (n%3===0){
+        return "julia"
+    }
+    else if (n%5===0){
+        return "james"
+    }
+    else if(n%3===0 && n%5===0){
+        return "juliajames"
+    }
+    else if(n%3!==0 || n%5!==0)
+        return n
+}
+
+}
+VM1100:3 Uncaught SyntaxError: Unexpected identifier 'let'
+function weirdDivisionWithFor(n) {
+for (let i = n; i < 1; i++) {
+    if (n%3===0){
+        return "julia"
+    }
+    else if (n%5===0){
+        return "james"
+    }
+    else if(n%3===0 && n%5===0){
+        return "juliajames"
+    }
+    else if(n%3!==0 || n%5!==0)
+        return n
+}
+
+}
+undefined
+weirdDivisionWithFor(0.3)
+0.3
+function weirdDivisionWithFor(n) {
+for (let i = n; i <=1; i++) {
+    if (n%3===0){
+        return "julia"
+    }
+    else if (n%5===0){
+        return "james"
+    }
+    else if(n%3===0 && n%5===0){
+        return "juliajames"
+    }
+    else if(n%3!==0 || n%5!==0)
+        return n
+}
+
+}
+undefined
+weirdDivisionWithFor(1)
+1
+weirdDivisionWithFor()
+undefined
+function weirdDivisionWithFor(n) {
+if(){
+    return
+}
+return
+
+}
+VM1291:2 Uncaught SyntaxError: Unexpected token ')'
+function  laugh(n){
+    var result=""
+    for (let i = 0; i <n; i++) {
+        result=result+i
+    }
+    return result
+}
+undefined
+laugh()
+''
+function  laugh(n){
+    var result="ha"
+    for (let i = 0; i <n; i++) {
+        result=result+i
+    }
+    return result
+}
+undefined
+laugh()
+'ha'
+function  laugh(n){
+    var result="ha"
+    for (let i = 0; i <n; i++) {
+        result=result*i
+    }
+    return result
+}
+undefined
+laugh()
+'ha'
+function  laugh(n){
+    var result="ha"
+    for (let i = 0; i <n; i++) {
+        result=result*array[i]
+    }
+    return result
+}
+undefined
+function  laugh(n){
+    var oo=array[]
+    var result="ha"
+    for (let i = 0; i <n; i++) {
+        result=result*array[i]
+    }
+    return result
+}
+VM1579:2 Uncaught SyntaxError: Unexpected token ']'
+function  laugh(n){
+    var oo=[]
+    var result="ha"
+    for (let i = 0; i <n; i++) {
+        result=result*array[i]
+    }
+    return result
+}
+undefined
+laugh()
+'ha'
+function  laugh(n){
+    var array="ha"
+    for (let i = 0; i <n; i++) {
+        array=array*array[i]
+    }
+    return array
+}
+undefined
+laugh()
+'ha'
+function reverseIterateAndLogRecursively(n){
+    if(n===0){
+        return 
+        
+    }if(n%2===0){
+        console.log(n+"is even")
+        
+    }
+    else {
+        console.log(n+"is odd")
+    }
+    return reverseIterateAndLogRecursively(n-1)
+}
+undefined
+reverseIterateAndLogRecursively(2)
+VM1860:6 2is even
+VM1860:10 1is odd
+undefined
+reverseIterateAndLogRecursively(20)
+VM1860:6 20is even
+VM1860:10 19is odd
+VM1860:6 18is even
+VM1860:10 17is odd
+VM1860:6 16is even
+VM1860:10 15is odd
+VM1860:6 14is even
+VM1860:10 13is odd
+VM1860:6 12is even
+VM1860:10 11is odd
+VM1860:6 10is even
+VM1860:10 9is odd
+VM1860:6 8is even
+VM1860:10 7is odd
+VM1860:6 6is even
+VM1860:10 5is odd
+VM1860:6 4is even
+VM1860:10 3is odd
+VM1860:6 2is even
+VM1860:10 1is odd
+undefined
